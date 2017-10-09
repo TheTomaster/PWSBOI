87be91df9315127b25a251244227e2eb869ebda4
TheTomaster <31759513+TheTomaster@users.noreply.github.com>
2017-10-09 14:23:14 +0200

    je kan nu kijken en bewegen

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -6,6 +6,8 @@ public class PlayerController : MonoBehaviour {
 
     [SerializeField]
     private float speed = 5f;
+    [SerializeField]
+    private float lookSensitivity = 3f;
 
     private PlayerMotor motor;
 
@@ -28,5 +30,18 @@ public class PlayerController : MonoBehaviour {
 
         //apply movement
         motor.Move(velocity);
+
+        float _yRot = Input.GetAxisRaw("Mouse X");
+
+        Vector3 _rotation = new Vector3(0f, _yRot, 0f) * lookSensitivity;
+
+        motor.Rotate(_rotation);
+
+        float _xRot = Input.GetAxisRaw("Mouse Y");
+
+        Vector3 _cameraRotation = new Vector3(_xRot, 0f, 0f) * lookSensitivity;
+
+        motor.RotateCamera(_cameraRotation);
+
     }
 }
